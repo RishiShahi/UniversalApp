a97112e2423ca0ef1f9c0c4e171188af2ed34c7a
suresh sharma <suresh.sharma510@gmail.com>
2022-04-29 10:45:00 +0530

    Suresh  counter is added

--- src/Modules/Suresh/Suresh.vue
+++ src/Modules/Suresh/Suresh.vue
@@ -1,3 +1,15 @@
+<script>
+  export default {
+    data(){
+      return {
+        count: 0,
+      }
+    }
+  }
+</script>
+
 <template>
-  <h1>Suresh</h1>
+  <h1>This is my interface ( Suresh )</h1>
+  <p>Count : <span v-html="count"></span></p>
+  <button @click="count++">Increment</button>
 </template>
